7e87b1293a1e1ced9d2a731234afddf3e074903a
NikulenkoE <kazjabbushka@gmail.com>
2014-03-13 16:46:37 +0300

    1

---
+++ src/main/java/suits/LoginPageTest.java
@@ -0,0 +1,20 @@
+package suits;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import screen.LoginPageScreen;
+
+public class LoginPageTest {
+
+	private static final String EXPECTED_TEXT_LOGIN_PAGE_HEADER = "Вход в систему";
+
+	@Test(description = "Проверяем наличие основных элементов на странице логина.")
+	public void simpleTest() throws InterruptedException {
+		LoginPageScreen loginPageScreen = new LoginPageScreen().openLoginPageScreen();
+		Assert.assertEquals(loginPageScreen.getHeaderText(), EXPECTED_TEXT_LOGIN_PAGE_HEADER, "Текст заголовка страницы логина"
+				+ " не соответствует ожидаемому.");
+		loginPageScreen.closeBrowser();
+	}
+
+}
